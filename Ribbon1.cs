e089fccc9aacdd2221b1d83e2291e9c9d757efe1
kaifeng <ykf@live.cn>
2022-11-19 17:56:39 +0800

    实现行距1.2倍功能

---
+++ Ribbon1.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Office.Tools.Ribbon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PowerPoint = Microsoft.Office.Interop.PowerPoint;
+using Office = Microsoft.Office.Core;
+using System.Runtime.InteropServices;
+
+namespace PPTools
+{
+    public partial class Ribbon1
+    {
+        PowerPoint.Application app;// 实例化当前PPT
+        private void Ribbon1_Load(object sender, RibbonUIEventArgs e)  //加载事件
+        {
+            app= Globals.ThisAddIn.Application;
+        }
+
+        private void button1_Click(object sender, RibbonControlEventArgs e)
+        {
+            PowerPoint.Selection sel = app.ActiveWindow.Selection;
+            sel.TextRange.ParagraphFormat.SpaceWithin = 1.2f;
+            if (sel.Type == PowerPoint.PpSelectionType.ppSelectionText) {//调整选中文字的行间距
+                //System.Windows.Forms.MessageBox.Show("ppSelectionText");
+                //sel.ShapeRange.TextFrame.TextRange.ParagraphFormat.SpaceWithin = 10;
+                sel.TextRange.ParagraphFormat.LineRuleWithin = Office.MsoTriState.msoTrue;
+                sel.TextRange.ParagraphFormat.SpaceWithin = 1.2f;
+                //PowerPoint.TextRange range= sel.TextRange;
+                //foreach (PowerPoint.TextRange shape in range)
+                //{
+                //    //shape.ShapeStyle = Office.MsoShapeStyleIndex.msoShapeStyleMixed;
+                //    //shape.Width= shape.Width*2;
+                //    //shape.Height= shape.Height*2;
+                //    //if (shape.Type == Office.MsoShapeType.msoTextBox)
+                //    //{
+                //    //    shape.TextEffect.FontSize = shape.TextEffect.FontSize + 1; 
+                //    //}
+
+                //    shape.Text = "fdfd0";
+
+
+
+                //}
+            }
+        }
+    }
+}
--- ThisAddIn.cs
+++ ThisAddIn.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Xml.Linq;
 using PowerPoint = Microsoft.Office.Interop.PowerPoint;
 using Office = Microsoft.Office.Core;
+using System.Runtime.InteropServices;
 
 namespace PPTools
 {
@@ -16,6 +17,16 @@ namespace PPTools
 
         private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
         {
+            //强制回收资源
+            //有时关闭Excel文件，都残留Excel.exe空进程。导致下次打开Excel文件，没有加载插件
+            //有时才会，不是每次都会，说明有时可以回收资源，有时回收不了
+            try
+            {
+                GC.Collect();
+                Marshal.FinalReleaseComObject(Globals.ThisAddIn.Application);
+                GC.Collect();
+            }
+            catch { }
         }
 
         #region VSTO 生成的代码
